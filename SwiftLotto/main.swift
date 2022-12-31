c487794f5142c2b38e2773eff0a4101bed811c1e
dhk767 <dhk767@gmail.com>
2022-12-31 13:49:14 +0900

    로또번호 추첨기 제작 완료

--- SwiftLotto/main.swift
+++ SwiftLotto/main.swift
@@ -7,5 +7,22 @@
 
 import Foundation
 
-print("Hello, World!")
+print("금주의 로또 추천번호는...")
+for _ in 0 ... 5 {
+    print(lottoArray(20000000))
+}
 
+func lottoArray(_ loopCount: Int) -> [Int] {
+    var answer = [Int](repeating: 0, count: 6)
+    var numberCount = [Int](repeating: 0, count: 45)
+    for _ in 0 ... loopCount {
+        numberCount[Int.random(in: 0 ... 44)] += 1
+    }
+    for i in 0 ... 5 {
+        var maxNumber = numberCount.max()!
+        var index = numberCount.firstIndex(of: maxNumber)!
+        answer[i] = index + 1
+        numberCount[index] = -1
+    }
+    return answer.sorted()
+}
